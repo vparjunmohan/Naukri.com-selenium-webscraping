3bc5f48415e2d5505fab91745fbd72fd6a78b8bc
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-05 18:39:12 +0530

    updated imports, saves scrapped data into csv file

--- naukri-webscraping.py
+++ naukri-webscraping.py
@@ -2,23 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from time import sleep   
+from selenium.webdriver.common.by import By 
+import csv
+from time import sleep
 
-search = input('Enter the job: ')
+search = input('Search Job: ')
+location = input('Job Location: ')
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
 driver = webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe', options=options)
 wait = WebDriverWait(driver, 20)
-driver.get(url=f'https://www.naukri.com/{search}-jobs?k={search}')
+driver.delete_all_cookies()
+driver.get(url=f'https://www.naukri.com/{search}-jobs?k={search}&l={location}')
 
-headings = driver.find_elements_by_xpath('//*[@class="title fw500 ellipsis"]')
-job_links = []
-for i in headings:
-    link = i.get_attribute('href')
-    job_links.append(link)
-print(job_links)
-print(len(job_links))
+csv_filename = input('Enter CSV file name: ')     # Creating CSV File
+csv_file = open(csv_filename+'.csv', 'a', encoding="utf-8", newline='')  # Opening CSV File
+
+csv_writer = csv.writer(csv_file)
 

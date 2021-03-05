011fddc9d0cc630d761be0c47395edcd1049b3ed
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-05 18:40:55 +0530

    updated file

--- naukri-webscraping.py
+++ naukri-webscraping.py
@@ -22,3 +22,105 @@ csv_file = open(csv_filename+'.csv', 'a', encoding="utf-8", newline='')  # Openi
 
 csv_writer = csv.writer(csv_file)
 
+# Header of CSV File
+csv_writer.writerow(['Job Title', 'Company Name', 'Experience', 'Salary', 'Location', 'Date Posted', 'Job Description', 'Role', 'Industry Type', 'Functional Area', 'Employment Type', 'Role', 'Education', 'Key Skills', 'About Company'])
+
+while True:
+    total_pages = driver.find_elements_by_xpath('//*[@class="fleft grey-text mr-5 fs12"]')
+    for i in total_pages:
+        page = i.text
+    print()
+    print('Scraping: ', page)
+
+    
+    job_titles = driver.find_elements_by_xpath('//*[@class="title fw500 ellipsis"]')
+    job_links = []                  # List of job links on a particular page
+    
+    for title in job_titles:
+        link = title.get_attribute('href')
+        job_links.append(link)
+
+    page_url = driver.current_url  # URL of the current page
+    
+    for link in job_links:         # Scraping individual job link on the particular page
+
+        driver.get(link)
+
+        titles = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[1]/div[1]/div[1]/header/h1')
+        for title in titles:
+            a = title.text             # Job title
+
+        names = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[1]/div[1]/div[1]/div/a[1]')
+        for name in names:
+            b = name.text              # Company Name
+
+        experiences = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[1]/div[1]/div[2]/div[1]/span')
+        for experience in experiences:
+            c = experience.text        # Year of Experience
+
+        salaries = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[1]/div[1]/div[2]/div[2]/span')
+        for salary in salaries:
+            d = salary.text            # Salary
+
+        locations = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[1]/div[1]/div[2]/div[3]/span')
+        for location in locations:
+            e = location.text          # Job location
+
+        dates = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[1]/div[2]/div[1]/span[1]/span')
+        for date in dates:
+            f = date.text              # Posted Date
+
+        descriptions = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[1]')
+        for description in descriptions:
+            g = description.text       # Job Description
+
+        roles = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[2]/div[1]/span')
+        for role in roles:
+            h = role.text              # Job Role  
+
+        industry_types = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[3]/div[2]/span')
+        for industry_type in industry_types:
+            i = industry_type.text     # Industry Type
+
+        functional_areas = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[2]/div[3]')
+        for functional_area in functional_areas:
+            j = functional_area.text   # Functional Area
+
+        employment_types = driver.find_elements(By.XPATH, 'html/body/div[1]/main/div[2]/div[2]/section[2]/div[2]/div[4]/span')
+        for employment_type in employment_types:
+            k = employment_type.text   # Employement Type
+
+        role_categories = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[2]/div[5]/span')
+        for role_category in role_categories:
+            l = role_category.text     # Role Category
+
+
+        educations = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[3]')
+        for education in educations:
+            m = education.text         # Education
+
+        skills = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[2]/div[4]')
+        for skill in skills:
+            n = skill.text             # Skills
+
+        company_details = driver.find_elements(By.XPATH, '/html/body/div[1]/main/div[2]/div[2]/section[4]')
+        for company_detail in company_details:
+            o = company_detail.text    # About Company
+
+            
+            csv_writer.writerow([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o])  # Writing all the scrapped data into CSV file.
+    
+    driver.get(page_url)           # Redirecting back to particular search result URL
+    del job_links                  # Deleting the job_links list of previous page
+    sleep(10)
+    try:
+        next_page = driver.find_element_by_xpath('//*[@class="fright fs14 btn-secondary br2"]')
+        next_page.click()          # Button click for the next page
+        sleep(10)
+    except Exception as e:
+        print(e)
+
+    page_url = driver.current_url  # URl of the currently opened page
+    
+    print('New Page URL: ', page_url)
+    print()
